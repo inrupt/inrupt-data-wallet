99760c678e6bb814068ce2c1ef4aa5677e064e4c
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-08-06 10:02:51 +0200

    Switch config based on environment

--- app.config.js
+++ app.config.js
@@ -64,7 +64,7 @@ export default {
         "expo-build-properties",
         {
           android: {
-            usesCleartextTraffic: false,
+            usesCleartextTraffic: process.env.NODE_ENV === "development",
           },
         },
       ],

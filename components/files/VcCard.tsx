5ea30eb4cc8bef711c70865cb864babf9b004052
Pete Edwards <edwardsph@users.noreply.github.com>
2024-08-09 13:05:31 +0100

    Convert number to string explicitly

--- components/files/VcCard.tsx
+++ components/files/VcCard.tsx
@@ -47,7 +47,7 @@ const VcCard: React.FC<IComponentProps> = ({ data }) => {
             title={addSpacesToCamelCase(subject[0])}
             content={
               typeof subject[1] === "string" || typeof subject[1] === "number"
-                ? (subject[1] as string)
+                ? subject[1].toString()
                 : ""
             }
           />

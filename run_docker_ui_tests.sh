73712d8c2a7eeeb32def2b1c1f0753b505eb5982
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-08-12 11:46:09 +0200

    Add hardware acceleration check

--- run_docker_ui_tests.sh
+++ run_docker_ui_tests.sh
@@ -3,6 +3,8 @@
 # Enable QEmu hardware acceleration.
 sudo chown circleci -R /dev/kvm
 echo "kvm:x:200:circleci" | sudo tee -a /etc/group
+# Check that hardware emulation is enabled.
+emulator -accel-check
 
 # Run the tests
 npx detox test --configuration android.emu.release --loglevel verbose

2b693fdd64cfacca485cdb197c86f88790e72e82
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-08-19 10:37:53 +0200

    Add jest config for snapshot tests

---
+++ jest.config.ts
@@ -0,0 +1,42 @@
+//
+// Copyright Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+import type { Config } from "@jest/types";
+import { createJsWithBabelPreset } from "ts-jest";
+
+const config: Config.InitialOptions = {
+  preset: "react-native",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    // "^.+\\.tsx?$": "ts-jest/presets/js-with-babel",
+    ...createJsWithBabelPreset().transform,
+  },
+  // The following must match the tsconfig paths.
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  rootDir: ".",
+  testMatch: ["<rootDir>/app/**/*.test.tsx"],
+  testTimeout: 120000,
+  maxWorkers: 1,
+  verbose: true,
+};
+
+export default config;
